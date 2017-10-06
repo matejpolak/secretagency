53d6cefcd876271179f2946483046eee9ab378f2
Matěj Polák <matpolak@seznam.cz>
2017-10-06 13:00:26 +0200

    basic login page with some basic login option added

--- login.php
+++ login.php
@@ -1,12 +1,23 @@
 <?php
 require 'db.php';
-$query = 'SELECT * FROM messages';
+$query = 'SELECT * FROM users';
 
 $stmt = db::query($query);
 
 // var_dump($stmt);
 
 $data = $stmt->fetchAll();
+
+
+if($_POST) {
+    // $username = $_POST['username'];
+    // $password = $_POST['password'];
+    foreach ($data as $users) {
+        if(($_POST['username'] == $users['username']) && ($_POST['password'] == $users['password'])) {
+                header('Location: cbp.php');
+        } 
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +25,7 @@ $data = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DSA</title>
+    <title>DSA | LOGIN</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body class="bg-secondary">
@@ -23,29 +34,22 @@ $data = $stmt->fetchAll();
     </nav>
     <div class="container">
         <div class="row d-flex justify-content-center">
-            <?php foreach($data as $id => $value) : ?>
-            <div class="col-10 mt-3">
-
-                <div class="card bg-dark text-light">
-                    <div class="card-header">
-                        <b><?= $value['user'];?></b> <small>(level :<?= $value['level'];?>)</small>
+            <div class="col-4 bg-dark mt-5  rounded">
+                <h2 class="text-light mt-2">Sign Up</h2>
+                <form class="pt-2 pb-1" method="post">
+                    <div class="form-group">
+                        <label class="text-light" for="username">Username:</label>
+                        <input type="text" name="username" class="form-control" id="username" placeholder="Enter your username">
                     </div>
-                    <div class="card-body d-flex justify-content-center">
-                    <a class="btn btn-primary" data-toggle="collapse" href="#collapse<?= $value['id'];?>" aria-expanded="false" aria-controls="collapse<?= $value['id'];?>">
-                        See the message content
-                    </a>
+                    <div class="form-group">
+                        <label class="text-light" for="password">Password:</label>
+                        <input type="password" name="password" class="form-control" id="password" placeholder="Enter your password">
                     </div>
-                    <div class="collapse" id="collapse<?= $value['id'];?>">
-                        <div class="card-body">
-                            <blockquote class="blockquote mb-0">
-                            <p><small><?= $value['text']; ?></small></p>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
+                    <button type="submit" class="btn btn-primary w-40">Sign In</button>
+                    <button type="submit" class="btn btn-primary w-40">Sign Up</button>
+                    <small id="emailHelp" class="form-text text-muted">Never share your login informations with anyone else.</small>
+                </form>
             </div>
-
-            <?php endforeach; ?>
         </div>
     </div>
 
