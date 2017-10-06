ed2f256b81e4be0e7d92e5f917d86d22956f5068
Matěj Polák <matpolak@seznam.cz>
2017-10-06 09:37:05 +0200

    secret agency page template

---
+++ cbp.php
@@ -0,0 +1,62 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DSA</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark d-flex justify-content-around">
+        <a class="navbar-brand text-center" href="#">DSA - Deep Secret Agency</a>
+    </nav>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-10 mt-3">
+                <div class="card bg-dark text-light">
+                    <div class="card-header">
+                        razerbalde (level 0)
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+            <div class="col-10 mt-3">
+                <div class="card bg-dark text-light">
+                    <div class="card-header">
+                        razerbalde (level 0)
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+            <div class="col-10 mt-3">
+                <div class="card bg-dark text-light">
+                    <div class="card-header">
+                        razerbalde (level 0)
+                    </div>
+                    <div class="card-body">
+                        <blockquote class="blockquote mb-0 border-info">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
