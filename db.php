9f7bc6e5d07dc63cffb6dc846dd2174d0e980eda
Matěj Polák <matpolak@seznam.cz>
2017-11-22 10:16:52 +0100

    deploy

--- db.php
+++ db.php
@@ -1,5 +1,60 @@
 <?php
 
-   $host='localhost';
-    $username='root';
-    $userpwd='rootroot';
+class db {
+    protected static $pdo = null;
+    protected static $host = 'localhost';
+    protected static $username = 'root';
+    protected static $password = 'rootroot';
+    protected static $database = 'imdb';
+
+    public static function pdo() {
+        if(static::$pdo === null) {
+            // connect to the database
+            try {
+                // store the connection (PDO) into static::$pdo
+                static::$pdo = new PDO(
+                // 'mysql:dbname=database_name;host=locahost;charset=utf8'
+                'mysql:dbname='.static::$database.';host='.static::$host.';charset=utf8', 
+                static::$username,
+                static::$password
+                );
+
+                // set error reporting
+                static::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } 
+            catch (PDOException $e) {
+                // if something went wrong, just print out the error message            
+                echo 'Connection failed: ' . $e->getMessage();
+            }
+        }
+        return static::$pdo;
+    }
+
+    protected function exitWithError() {
+        // print a <h1>
+        echo '<h1>MySQL error:</h1>';
+    
+        // dump information about the error
+        var_dump(static::pdo()->errorInfo());
+    
+        // end execution
+        exit();
+    }
+
+    public static function query($sql, $substitutions = []) {
+        // get PDO connection object
+        $pdo = static::pdo();
+        // prepare a statement out of SQL
+        $stmt = $pdo->prepare($sql);
+        // we run the query and keep the outcome (true or false)
+        $outcome = $stmt->execute($substitutions);
+        // if there was an error
+        if($outcome === false) {
+            // print the error and exit
+            static::exitWithError();
+        }
+        // return the statement (pointing to the result)
+        return $stmt;
+    }
+
+}
--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,8 @@ function db_connect() {
     global $host;
     global $username;
     global $userpwd;
-    $db = new PDO('mysql:host=' . $host . ';dbname=messages;charset=utf8', $username, $userpwd);
+    global $database;
+    $db = new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8', $username, $userpwd);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $db;
 }
